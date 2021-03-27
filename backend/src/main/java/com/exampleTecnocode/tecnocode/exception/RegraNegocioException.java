d1ce9a4bed837f84d1136ce6b18982d305eec0e9
ODAGAMMXIX <54047352+ODAGAMMXIX@users.noreply.github.com>
2021-03-27 16:54:20 -0300

    clean-up
    4 presentation

--- backend/src/main/java/com/exampleTecnocode/tecnocode/exception/RegraNegocioException.java
+++
@@ -1,9 +0,0 @@
-package com.exampleTecnocode.tecnocode.exception;
-
-public class RegraNegocioException extends RuntimeException {
-	
-	public RegraNegocioException(String msg) {
-		super(msg);
-	}
-
-}
