2dc828780fd9c280a012ed42c914a970348793d6
ODAGAMMXIX <54047352+ODAGAMMXIX@users.noreply.github.com>
2021-03-27 16:14:56 -0300

    CLEANING-UP-OLD-VERSIONS

--- front/src/services/links.js
+++
@@ -1,9 +0,0 @@
-export function Links (link){
-
-    const listLinks = {
-        dashboard: {to: '/dashboard', label: 'Dashboard'},
-        config: {to: '/config', label: 'Configurações'},
-    }
-
-    return listLinks[link]
-}
