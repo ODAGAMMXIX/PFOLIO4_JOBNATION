f0f9b45c248db9b3b93d1169f546a9aba241b464
CarolPaz <caroline.paz.sousa@outlook.com>
2021-04-18 16:44:10 -0300

    Commit Language

--- backend/src/main/java/com/tecnocode/controller/LanguageController.java
+++ backend/src/main/java/com/tecnocode/controller/LanguageController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,4 +46,11 @@ public class LanguageController {
 	public List<Language> buscarTodosComEsteNivel(@PathVariable("level") String level) {
 		return service.buscarTodosComEsteNivel(level);
 	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity delete(@PathVariable("id") Integer id) {
+		service.delete(id);
+		return ResponseEntity.status(HttpStatus.OK).build();
+	}
+
 }
