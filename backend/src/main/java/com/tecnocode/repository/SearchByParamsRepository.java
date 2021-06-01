f411e54cc44417dc4768aa48c0c2f810b0729fb2
Rafael Estevam de Oliveira <rafael.estevamdesign@gmail.com>
2021-05-31 22:09:50 -0300

    ajust of repository search by params

--- backend/src/main/java/com/tecnocode/repository/SearchByParamsRepository.java
+++ backend/src/main/java/com/tecnocode/repository/SearchByParamsRepository.java
@@ -11,38 +11,32 @@ import java.util.List;
 
 @Repository
 public interface SearchByParamsRepository extends JpaRepository<Candidate, Integer> {
-    @Query(value = "Select candidate_profile.* " +
-            "        from candidate_profile, experience, candidate_experience, " +
-            "        (SELECT * FROM TABLE(get_address_by_lat_long(?2, ?3, ?4))) endereco where ?1", nativeQuery = true)
-    List<Object> searchByParams(String query,
-                                Float lat,
-                                Float lng,
-                                Integer range_distance);
+//    @Query(value = "Select candidate_profile.* " +
+//            "        from candidate_profile, experience, candidate_experience, " +
+//            "        (SELECT * FROM TABLE(get_address_by_lat_long(?2, ?3, ?4))) endereco where ?1", nativeQuery = true)
+//    List<Object> searchByParams(String query,
+//                                Float lat,
+//                                Float lng,
+//                                Integer range_distance);
 
 
-    @Query(
+    @Query(value=
             "SELECT distinct candidate.firstName " +
                     "FROM Candidate candidate " +
-                    "JOIN candidate.experiences experiences " +
-                    "JOIN candidate.qualifications qualifications " +
-                    "JOIN candidate.skills skills " +
-                    "JOIN candidate.languages languages " +
-                    "WHERE (:levels is null or qualifications.level IN (:levels))" +
-                    "AND (:skils is null or skills.name IN (:skils))" +
-                    "AND (:languages is null or languages.name IN (:languages))" +
-                    "AND (:gender is null or candidate.gender = :gender)" +
-                    "AND (:exp is null or (to_date(experiences.end,'dd/MM/yyyy') - to_date(experiences.start,'dd/MM/yyyy')) >= :exp)" +
-                    "AND (:range is null or candidate.address  get_address_by_lat_long(:lat, :lng, :range))"
+                    "JOIN candidate.experience experience " +
+                    "JOIN candidate.qualification qualification " +
+                    "JOIN candidate.skill skill " +
+                    "JOIN candidate.language language " +
+                    "WHERE (?1 is null or qualification.level IN (?1))" +
+                    "AND (?2 is null or skill.name IN (?2))" +
+                    "AND (?3 is null or language.name IN (?3))" +
+                    "AND (?4 is null or candidate.gender = ?4)", nativeQuery = true
     )
-    List<String> searchAllCandidates(
-            @Param("levels") List<String> levels,
-            @Param("skils") List<String> skils,
-            @Param("languages") List<String> languages,
-            @Param("gender") String gender,
-            @Param("exp") Double exp,
-            @Param("lat") Double lat,
-            @Param("lng") Double lng,
-            @Param("range") Integer range
+    List<Object> searchAllCandidates(
+            List<String> levels,
+            List<String> skills,
+            List<String> languages,
+            String gender
     );
 
 }
